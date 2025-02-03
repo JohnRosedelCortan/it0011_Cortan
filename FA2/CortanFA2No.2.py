33ee7412cf5109d238657e8bbc6027625cdf0f1a
JohnRosedelCortan <johncortan37@gmail.com>
2025-02-03 10:37:36 +0800

    10:37 pm - 02/03/25

---
+++ FA2/CortanFA2No.2.py
@@ -0,0 +1,13 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+
+upper_case_name = full_name.upper()
+lower_case_name = full_name.lower()
+
+name_length = len(full_name)
+
+print("\nFull Name:", full_name)
+print("\Full Name (Upper Case):", upper_case_name)
+print("\Full Name (Lower Case):", lower_case_name)
+print("Length of Full Name:", name_length)
